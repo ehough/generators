9911ed44b97b507af14d52158ef58a1990555d0a
Eric Hough <eric@tubepress.com>
2016-12-31 13:09:04 -0800

    Happy New Year!

--- src/ArrayPoppingGenerator.php
+++ src/ArrayPoppingGenerator.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2016 Eric D. Hough (https://github.com/ehough)
+ * Copyright 2016 - 2017 Eric D. Hough (https://github.com/ehough)
  *
  * This file is part of ehough/generators (https://github.com/ehough/generators)
  *
